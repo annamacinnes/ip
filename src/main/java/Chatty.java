da71c7ee753d8d7a1072dd2d834bdbbcd1459a7d
anna-macinnes <anna.macinnes@galacticroam.com>
2026-01-22 15:40:49 +0800

    Level 2

--- src/main/java/Chatty.java
+++ src/main/java/Chatty.java
@@ -5,13 +5,21 @@ public class Chatty {
         System.out.println("Hello! I'm Chatty");
         System.out.println("What can I do for you?");
         Scanner sc = new Scanner(System.in);
+        String[] storage = new String[100];
+        int current = 0;
         while (true) {
             String inpt = sc.nextLine();
-            if (inpt.equals("Bye") || inpt.equals("bye")) {
+            if (inpt.equalsIgnoreCase("bye")) {
                 System.out.println("Bye. Hope to see you again!");
                 break;
+            } else if (inpt.equalsIgnoreCase("list")) {
+                for (int i = 0; i < current; i++) {
+                    System.out.printf("%d: %s%n", i + 1, storage[i]);
+                }
             } else {
-                System.out.println(inpt);
+                storage[current] = inpt;
+                current++;
+                System.out.printf("added: %s%n", inpt);
             }
         }
         sc.close();
