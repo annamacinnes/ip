5b2c956ea6dcbf0e39ace673abbdd4d5399ac24d
anna-macinnes <anna.macinnes@galacticroam.com>
2026-01-22 15:27:42 +0800

    Level 1

--- src/main/java/Chatty.java
+++ src/main/java/Chatty.java
@@ -1,5 +1,19 @@
-public class Duke {
+import java.util.Scanner;
+
+public class Chatty {
     public static void main(String[] args) {
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Chatty");
+        System.out.println("What can I do for you?");
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String inpt = sc.nextLine();
+            if (inpt.equals("Bye") || inpt.equals("bye")) {
+                System.out.println("Bye. Hope to see you again!");
+                break;
+            } else {
+                System.out.println(inpt);
+            }
+        }
+        sc.close();
     }
 }
